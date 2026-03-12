103cb6e42d8a450490d35d69913daacf20c66b9c
ugurnusretoglu <ugurnusretoglu2004@gmail.com>
2026-03-12 15:15:48 +0300

    feat: add todo update functionality

--- todoApp/src/App.jsx
+++ todoApp/src/App.jsx
@@ -15,11 +15,21 @@ function App() {
     setTodos([...todos.filter((todo) => todo.id !== todoId)])
   }
 
+  const updateTodo = (newTodo) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id !== newTodo.id) {
+        return todo;
+      }
+      return newTodo;
+    })
+    setTodos([...updatedTodos])
+  }
+
   return (
     <div className='App'>
       <div className='main'>
         <TodoCreate onCreateTodo={createTodo} />
-        <TodoList todos={todos} onRemoveTodo={removeTodo} />
+        <TodoList todos={todos} onRemoveTodo={removeTodo} onUpdateTodo={updateTodo} />
       </div>
     </div>
   )
--- todoApp/src/components/Todo.jsx
+++ todoApp/src/components/Todo.jsx
@@ -1,23 +1,43 @@
 import React from 'react'
 import { CiCircleRemove } from "react-icons/ci";
 import { MdEdit } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
 import '../App.css';
+import { useState } from 'react';
 
-function Todo({ todo, onRemoveTodo }) {
+function Todo({ todo, onRemoveTodo, onUpdateTodo }) {
     const { id, content } = todo;
+
+    const [editable, setEditable] = useState(false);
+    const [newTodo, setNewTodo] = useState(content);
 
     const removeTodo = () => {
         onRemoveTodo(id);
     }
 
+    const updateTodo = () => {
+        const request = {
+            id: id,
+            content: newTodo
+        }
+        onUpdateTodo(request);
+        setEditable(false);
+    }
+
     return (
         <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', border: '1px solid lightgrey', marginTop: '10px', marginTop: '10px' }}>
             <div>
-                {content}
+                {
+                    editable ? <input value={newTodo} onChange={(e) => setNewTodo(e.target.value)} style={{ width: '380px' }}
+                        className='todo-input' type="text" /> : content
+                }
             </div>
             <div>
                 <CiCircleRemove className='todo-icons' onClick={removeTodo} />
-                <MdEdit className='todo-icons' />
+
+                {
+                    editable ? <FaCheck className='todo-icons' onClick={updateTodo} /> : <MdEdit className='todo-icons' onClick={() => setEditable(true)} />
+                }
             </div>
         </div>
     )

c47ad091a6f0bc4da018b9e06ae093a7bc2b504d
ugurnusretoglu <ugurnusretoglu2004@gmail.com>
2026-03-24 16:18:25 +0300

    feat: add user logout functionality

--- firebaseProject/src/components/Navbar.jsx
+++ firebaseProject/src/components/Navbar.jsx
@@ -1,11 +1,28 @@
 import React from 'react'
 import '../CSS/Navbar.css'
+import { useNavigate } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { signOut } from 'firebase/auth'
+import { auth } from '../Firebase'
 
 function Navbar() {
+
+    const navigate = useNavigate()
+
+    const logout = async () => {
+        toast.success("Logging out")
+        setTimeout(() => {
+            signOut(auth)
+            navigate("/auth")
+        }, 3000)
+
+
+    }
+
     return (
         <div className='navbar'>
             <div className='navbar-left'>Firebase</div>
-            <div className='navbar-right'>Exit</div>
+            <div onClick={logout} className='navbar-right'>Exit</div>
         </div>
     )
 }

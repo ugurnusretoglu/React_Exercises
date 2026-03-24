d77c577c9327a64df7a2c35948b94e23a2731dfc
ugurnusretoglu <ugurnusretoglu2004@gmail.com>
2026-03-24 16:46:48 +0300

    feat: add Google sign-in with Firebase

--- firebaseProject/src/pages/Auth.jsx
+++ firebaseProject/src/pages/Auth.jsx
@@ -3,14 +3,30 @@ import '../CSS/Auth.css';
 import { FaGoogle } from "react-icons/fa";
 import { toast } from 'react-toastify';
 import { auth } from '../Firebase';
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { useNavigate } from 'react-router-dom';
+
+const provider = new GoogleAuthProvider();
 
 function Auth() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
+
+    const loginWithGoogle = async () => {
+        try {
+            const response = await signInWithPopup(auth, provider);
+            // const credential = GoogleAuthProvider.credentialFromResult(response);
+            // const token = credential.accessToken;
+            const user = response.user;
+            if (user) {
+                navigate("/")
+            }
+        } catch (error) {
+            toast.error(error.message)
+        }
+    }
 
     const login = async () => {
         try {
@@ -46,7 +62,7 @@ function Auth() {
                 <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Enter to password' className='input' />
             </div>
             <div>
-                <button className='google-button' > <FaGoogle style={{ marginRight: '5px' }} />
+                <button onClick={loginWithGoogle} className='google-button' > <FaGoogle style={{ marginRight: '5px' }} />
                     Sign in with Google</button>
                 <button onClick={login} className='login-button'>Log in</button>
                 <button onClick={register} className='register-button'>Sign up</button>
